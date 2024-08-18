efcab8f97e58cc796fc1e984b9a73f8bd784c999
Christopher Jimenez <jimenezchristopher5@gmail.com>
2024-08-18 00:14:59 -0500

    created a ball for pong game and refactored name of paddles to left and right

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 from paddle_class import Paddle
 
 BG_COLOR = "black"
@@ -11,16 +11,19 @@ screen.bgcolor(BG_COLOR)
 screen.title("Pong")
 screen.tracer(0)
 
-paddle_one = Paddle()
-paddle_one.set_position(350)
-paddle_two = Paddle()
-paddle_two.set_position(-350)
+paddle_right = Paddle(350)
+paddle_left = Paddle(-350)
+
+ball = Turtle(shape="circle")
+ball.color("white")
+ball.goto(x=380, y=280)
+
 
 screen.listen()
-screen.onkey(fun=paddle_one.up, key="Up")
-screen.onkey(fun=paddle_one.down, key="Down")
-screen.onkey(fun=paddle_two.up, key="w")
-screen.onkey(fun=paddle_two.down, key="s")
+screen.onkey(fun=paddle_right.up, key="Up")
+screen.onkey(fun=paddle_right.down, key="Down")
+screen.onkey(fun=paddle_left.up, key="w")
+screen.onkey(fun=paddle_left.down, key="s")
 
 
 game_is_on = True
--- paddle_class.py
+++ paddle_class.py
@@ -6,15 +6,13 @@ STRETCH_LEN = 1
 PADDLE_COLOR = "white"
 
 class Paddle(Turtle):
-    def __init__(self):
+    def __init__(self, x_axis):
         super().__init__()
         self.shape("square")
         self.shapesize(stretch_wid=STRETCH_WID,stretch_len=STRETCH_LEN)
         self.color(PADDLE_COLOR)
-        self.penup()
-
-    def set_position(self, x_axis):
         self.teleport(x=x_axis, y=0)
+        self.penup()
 
     def up(self):
         if self.ycor() < 260:
