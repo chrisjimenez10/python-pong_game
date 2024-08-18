540be18c1ac25cdc313e6a70f77fcd84e6008049
Christopher Jimenez <jimenezchristopher5@gmail.com>
2024-08-17 21:00:17 -0500

    created screen setup for pong game

---
+++ main.py
@@ -0,0 +1,27 @@
+from turtle import Screen
+
+BG_COLOR = "black"
+WIDTH = 800
+HEIGHT = 600
+
+screen = Screen()
+screen.setup(width=WIDTH, height=HEIGHT)
+screen.bgcolor(BG_COLOR)
+screen.title("Pong")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
